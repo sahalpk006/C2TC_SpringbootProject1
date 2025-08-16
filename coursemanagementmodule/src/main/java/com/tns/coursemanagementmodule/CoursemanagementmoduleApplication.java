5808101444a1fe068ae45fc379c4c0990a32e866
sahal <sahal@Sahalpk>
2025-08-16 13:12:53 +0530

    comit

---
+++ coursemanagementmodule/src/main/java/com/tns/coursemanagementmodule/CoursemanagementmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.tns.coursemanagementmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemanagementmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemanagementmoduleApplication.class, args);
+	}
+
+}
