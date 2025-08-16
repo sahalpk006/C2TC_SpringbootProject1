63f6fd792b968e7330d70999d0cbed37c6288273
sahal <sahal@Sahalpk>
2025-08-16 13:18:02 +0530

    COMIT

---
+++ manytomanymapping/src/test/java/com/tns/manytomanymapping/ManytomanymappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.manytomanymapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytomanymappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
